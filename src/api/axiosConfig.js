1c8c24f7b2e351ba9b54a2e0455264a91ba431d4
Paulo Souza <paulosouza.ti@gmail.com>
2024-01-04 15:02:24 -0300

    fix: log env

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log(process.env)
+
 export default axios.create({
     baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRD_SAAPATONA_API_URL : process.env.REACT_APP_DEV_SAAPATONA_API_URL
 })
